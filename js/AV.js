c10d9a39af3d7db48bff8fa8b6665d5a03aff02a
Nick Navarro <nicknavarro2012.com>
2021-09-08 22:29:43 -0430

    mi primer commit

---
+++ js/AV.js
@@ -0,0 +1,34 @@
+const nombre = document.getElementById("name")
+/*const email = document.getElementById("email")*/
+const pass = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warning")
+
+form.addEventListener("submit", e=>{
+  e.preventDefault()
+  let warning = ""
+  let entrar = false
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  parrafo.innerHTML = ""
+  /*if (nombre.value.length <6){
+    warning += 'El nombre no es valido <br>'
+    entrar = true
+  }*/
+
+  if(!regexEmail.test(email.value)){
+    warning += 'El email no es valido <br>'
+    entrar = true
+  }
+
+  if(pass.value.length <8){
+    warning += 'La contraseña no es valido <br>'
+    entrar = true
+  }
+
+  if(entrar){
+    parrafo.innerHTML = warning
+  }else{
+    parrafo.innerHTML = 'Enviado'
+  }
+  
+})
